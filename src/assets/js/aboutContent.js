2c64505dee92c3edbc1de865a465cacb6bd3642c
KostyaIliouk <kostya.iliouk@hotmail.com>
2020-08-29 21:06:58 -0400

    completed about page... now have blog page to do

--- src/assets/js/aboutContent.js
+++ src/assets/js/aboutContent.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-    setInterval(function() {
         var docHeight = $(window).height();
         var footerHeight = $('#footer').height();
         var navHeight = $('#mainNav').height() + $('#cvNav').height();
         $("#aboutContent").css('height',docHeight - navHeight - footerHeight - 25);
-    }, 250);
 });
